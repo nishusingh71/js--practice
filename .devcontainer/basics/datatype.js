df3d66809e8807d4ac9f4d1c25b4e4c1aba692f8
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2023-11-01 09:38:00 +0000

    dataType

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"; 
+console.log(3+3)
+console.log("Nishu")
+let name="nishu"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number=>2 to power 53
+//bigint
+//String=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof null);
+console.log(typeof undefined);
