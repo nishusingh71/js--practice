a25c2d30df3592f75ec3dab1bf0287c9cd024b46
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2023-11-01 09:20:38 +0000

    variable

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,15 @@
+const accountId=12345
+let accountEmail="nishu.gmai;.com"
+let accountPass="1234"
+accountCity="noida"
+/*
+prefer not to use var
+because scope variable 
+*/
+
+// accountId=2
+accountEmail="hc@hc.com"
+accountPass="2121"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity]);
