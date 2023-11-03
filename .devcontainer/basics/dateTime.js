36bcd5485587a8f36e3a41725c0e183b2df13c64
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2023-11-03 11:25:13 +0000

    dates

---
+++ .devcontainer/basics/dateTime.js
@@ -0,0 +1,9 @@
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(typeof myDate);
+
+let myCreatedDate=new Date(2023,0,23)
+console.log(myCreatedDate.toString());
